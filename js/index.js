ea32d331ab52d5b09af5d0f8e2e2118ec385aafc
Vlad <vladcyberit@gmail.com>
2025-05-11 19:29:52 +0300

    Getting elements from HTML

--- js/index.js
+++ js/index.js
@@ -1131,3 +1131,24 @@ function transferWaitors(data) {
 }
 
 transferWaitors(restorantData);
+
+//Getting elements from HTML
+
+const box = document.getElementById("box");
+console.log(box);
+
+const btns = document.getElementsByTagName("button");
+console.log(btns[0]);
+
+const circles = document.getElementsByClassName("circle");
+console.log(circles);
+
+const hearts = document.querySelectorAll(".heart");
+console.log(hearts);
+
+hearts.forEach(item => {
+    console.log(item);
+});
+
+const oneHeart = document.querySelector(".heart");
+console.log(oneHeart);
